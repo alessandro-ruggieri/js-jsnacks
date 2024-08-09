9b791f050580a14dc30dda2ba0fd13a8943134e4
alessandro-ruggieri <alessandro.ruggieri95.ar@gmail.com>
2024-08-09 05:02:03 +0200

    ADD: Three different solutions for snack-03.js

--- js/snack-03.js
+++ js/snack-03.js
@@ -1,11 +1,47 @@
 // Il software deve chiedere per 10 volte all’utente di inserire un numero.
 // Il programma stampa la somma di tutti i numeri inseriti.
 
-let num = 0
-let somma = 0
+//SOLUZIONE 1
 
-for (i=0; i<10; i++) {
-   num = prompt('Inserisci un numero')
-   somma = somma + parseFloat(num)
-}
-console.log('La somma dei numeri inseriti è ' + somma)
+      /*
+      let num = 0
+      let somma = 0
+
+      for (i=0; i<10; i++) {
+         num = prompt('Inserisci un numero')
+         somma = somma + parseFloat(num)
+      }
+      console.log('La somma dei numeri inseriti è ' + somma)
+      */
+
+//SOLUZIONE 2
+
+      /*
+      let num = 0
+      let somma = 0
+
+      for (i=0; i<10; i++) {
+         num = prompt('Inserisci un numero')
+         if (isNaN(num)) {
+            console.log('Il numero ' + num + ' non è valido')
+         } else {
+            somma = somma + parseFloat(num)
+         }
+      }
+      console.log('La somma dei numeri inseriti è ' + somma)
+      */
+
+// SOLUZIONE 3
+
+      let num = 0
+      let somma = 0
+
+      for (i=0; i<10; i++) {
+         num = prompt('Inserisci un numero')
+         while (isNaN(num)) {
+            console.log('Il numero ' + num + ' non è valido')
+            num = prompt('Il numero inserito non è valido. Inserisci un nuovo numero')
+         }
+         somma = somma + parseFloat(num)
+      }
+      console.log('La somma dei numeri inseriti è ' + somma)
